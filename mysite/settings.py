ede6c596a7921eef000cdaa10b3ac86b57932864
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-08-26 07:08:23 -0300

    add a simple search with postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     'blog.apps.BlogConfig',
     'taggit',
 ]
@@ -90,8 +91,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'PORT': '5432',
     }
 }
 
